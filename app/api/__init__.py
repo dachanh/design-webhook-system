0a35ed16ab8dc7e7941e4f14a98ae499b28f17cf
dachanh <ghostcodert7@gmail.com>
2024-02-22 16:31:25 +0700

    update

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,12 @@
 from flask import request
 from flask_restful import Resource
+from werkzeug.utils import secure_filename
+
 from schema.webhook import WebhookRegisterRequest
 from controllers.webhook_register import registerWebhookController
 from core.factory import factoryApp
 from core.response import error_response
+
 factory_app = factoryApp()
 
 class UploadFile(Resource):
@@ -12,4 +15,9 @@ class UploadFile(Resource):
             if 'file' not in request.files:
                 return {'message': 'No file part'}, 400
             file = request.files['file']
+
+            if file.filename == '':
+                 return  {'message': 'No selected file'}, 400
             
+            if file:
+                 filename = secure_filename(file.filename)
