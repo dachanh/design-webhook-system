ee1ab55cd76fe4a667be2817ffd852779b3ed7d0
dachanh <ghostcodert7@gmail.com>
2024-02-25 12:49:35 +0700

    update

--- app/core/factory.py
+++ app/core/factory.py
@@ -8,7 +8,8 @@ class factoryApp(object):
     def __init__(self) -> None:
         self.session = self.get_session_local()
         self.celery = self.make_celery()
-        self.upload_file = os.getenv("UPLOAD_FILE")
+        self.upload_file = os.getenv("UPLOAD_FILE",'./temp')
+        os.makedirs(self.upload_file,exist_ok=True)
 
     def get_session_local(self) -> sessionmaker:
         engine = create_engine(os.getenv("DATABASE_URL"))
@@ -19,7 +20,6 @@ class factoryApp(object):
         broker_url = os.getenv('CELERY_BROKER_URL', 'redis://127.0.0.1:6379/0')
         backend_url = os.getenv('CELERY_RESULT_BACKEND', 'redis://127.0.0.1:6379/0')
         celery = Celery('your_app_name', broker=broker_url, backend=backend_url)
-        print(backend_url)
         # Additional Celery configuration can go here
         celery.conf.update(task_serializer='json')
 
--- app/task/upload_file.py
+++ app/task/upload_file.py
@@ -4,7 +4,7 @@ from core.factory import factoryApp
 from schema.upload_file import UploadFileEvent
 from schema.webhook import WebhookExecute
 factory = factoryApp()
-event = factory.celery
+celery = factory.celery
 
 
 
@@ -13,16 +13,16 @@ class UploadFileTask(object):
     def __init__(self) -> None:
         pass
 
-    @event.task(bind=True)
+    @celery.task(bind=True)
     def publish_upload_file(self,data: dict):
         input:UploadFileEvent = UploadFileEvent(**data)
         file_path = os.path.join(factory.upload_file,input.filename)
-        
-        with open(file_path,"'wb") as file:
+
+        with open(file_path,"w") as file:
             file.write(input.file_content)
         
         return "ok"
-    @event.task
+    @celery.task(bind=True)
     def webhook(self,data: dict):
         input: WebhookExecute = WebhookExecute(**data)
         response =  requests.post(input.url)
