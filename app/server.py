08d9bce0e96390c1d2856ed63f4cea0d3966b55e
dachanh <ghostcodert7@gmail.com>
2024-02-26 03:53:53 +0700

    update

--- app/server.py
+++ app/server.py
@@ -14,7 +14,8 @@ def route_upload_file():
     upload_file_api = Api(app=app)
     upload_file_api.add_resource(UploadFileAPI, "/upload_file/user_id/<user_id>")
 
+route_webhook()
+route_upload_file()
+
 if __name__ == "__main__":
-    route_webhook()
-    route_upload_file()
     app.run(host="0.0.0.0", port=8081, debug=True)
